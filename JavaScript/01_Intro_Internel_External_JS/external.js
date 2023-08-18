bec491308cb705bfff5799f4678060058c9984a6
yunus <yunuspolat2528@gmail.com>
2023-08-18 16:49:19 +0200

    variables-operators

---
+++ JavaScript/01_Intro_Internel_External_JS/external.js
@@ -0,0 +1,18 @@
+
+console.log("externalden selamlar");
+
+console.error("error var!!!");
+
+console.warn("Bu bir uyaridir.");
+
+console.log(prompt("Lütfen Adinizi Giriniz"));
+
+// document.write("I'm in WebPage");
+
+window.alert("It is a message from WebPage");
+
+let age = prompt("How old are you?", 100);
+alert(`You are ${age} years old!`);
+
+let isAdult = confirm("Are you of legal age?");
+alert(isAdult);
